e61cbf1e7bd28cfa9ad97a37b2e921d9fa42f239
saarakhk <saara.vallik@khk.ee>
2020-04-04 18:39:29 +0300

    14. Use the Home Page Query in HackerBook Review App
    14. Use the Home Page Query in HackerBook Review App

--- hackerbookreview_4/web/src/Home.js
+++ hackerbookreview_4/web/src/Home.js
@@ -3,7 +3,44 @@ import * as R from 'ramda';
 import { BookListSection, SORT_BY } from './components/Book';
 import Error from './components/Error';
 import { RecentReviewSection } from './components/Review';
-import data from './data/';
+// import data from './data/';
+import fetch from './fetch';
+
+const query = `
+fragment Book on Book {
+  id
+  title
+  description
+  rating
+}
+
+fragment Review on Review {
+  id
+  title
+  rating
+  comment
+  user {
+    name
+  }
+}
+
+query Home($orderBy: BooksOrderBy!) {
+  reviews {
+    ...Review
+    book {
+      ...Book
+      imageUrl(size: SMALL)
+    }
+  }
+  books (orderBy: $orderBy) {
+    ...Book
+    imageUrl
+    authors {
+      name
+    }
+  }
+}
+`;
 
 class Home extends Component {
   state = {
@@ -20,12 +57,14 @@ class Home extends Component {
   }
   async loadData() {
     try {
-      // TODO: query actual books and reviews from graphql
-      const books = data.books;
-      const reviews = data.reviews;
-      const errors = [];
-      // eslint-disable-next-line
       const { orderBy } = this.state;
+      const variables = { orderBy };
+      const result = await fetch({ query, variables });
+      // const books = result.data.books;
+      const books = R.path(['data', 'books'], result);
+      const reviews = R.path(['data', 'reviews'], result);
+      const errorList = R.pathOr([], ['errors'], result);
+      const errors = R.map(error => error.message, errorList);
       this.setState({
         books,
         reviews,
